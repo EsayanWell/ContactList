1834052dea484843b827b1490af18d1a1dc40ee0
esayan_well <esayan_well@mail.ru>
2023-10-03 13:22:45 +0200

    first commit

---
+++ ContactList/ContactListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Владимир Есаян on 03.10.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ContactListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
