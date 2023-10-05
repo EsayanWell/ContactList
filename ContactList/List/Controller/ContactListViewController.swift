f03637d19edb63dbc12b9f7570978e6ed61becb4
esayan_well <esayan_well@mail.ru>
2023-10-05 15:59:39 +0200

    add textField

--- ContactList/List/Controller/ContactListViewController.swift
+++ ContactList/List/Controller/ContactListViewController.swift
@@ -23,23 +23,28 @@ class ContactListViewController: UIViewController {
     private func searchSetup() {
         view.addSubview(searchTextField)
         // скругление поисковой строки
-       // searchTextField.borderStyle = .roundedRect
         searchTextField.layer.cornerRadius = 16
-        //searchTextField.backgroundColor = .white
-        // cоздание иконки лупы
-        let seacrhIcon = UIImageView(image: UIImage(named: "search"))
-        // размер иконки
-        seacrhIcon.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        searchTextField.borderStyle = .none
+        searchTextField.layer.backgroundColor = UIColor(red: 0.969, green: 0.969, blue: 0.973, alpha: 1).cgColor
+        // cоздание иконок для textField
+        let searchIcon = UIImageView(image: UIImage(named: "search"))
+        let sortIcon = UIImageView(image: UIImage(named: "list-ui-alt"))
+        
+        // отступ
         // устанавливаем изображение внутри TextField
-        searchTextField.leftView = seacrhIcon
+        searchTextField.leftView = searchIcon
+        searchTextField.rightView = sortIcon
+        
         // режим отображения
-        searchTextField.leftViewMode = .unlessEditing
+        searchTextField.leftViewMode = .always
+        searchTextField.rightViewMode = .always
+        searchTextField.font = UIFont(name: "Inter-Medium", size: 15)
         // задаем цвет текст в placeholder и текст
-        searchTextField.attributedPlaceholder = NSAttributedString(string: "Введи имя, тег, почту ...", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.76, green: 0.76, blue: 0.78, alpha: 1)])
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Введи имя, тег, почту...", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 0.76, green: 0.76, blue: 0.78, alpha: 1)])
         // constraits
         searchTextField.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-            make.leading.equalToSuperview().offset(16) // Примерное смещение слева
+            make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(40)
         }
@@ -47,4 +52,3 @@ class ContactListViewController: UIViewController {
     }
     
 }
-
